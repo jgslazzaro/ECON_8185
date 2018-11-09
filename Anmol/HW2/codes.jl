2edba8379e369f0eb7df0311c28b0410a1a0c904
jgslazzaro <jgslazzaro@gmail.com>
2018-11-08 18:07:51 -0600

    The ellen HW 2 part works!

--- Anmol/HW2/codes.jl
+++ Anmol/HW2/codes.jl
@@ -2,27 +2,28 @@ using Plots, NLsolve, ForwardDiff, DataFrames, LinearAlgebra
 cd("C:\\Users\\jgsla\\Google Drive\\ECON_8185\\Anmol\\HW2")
 
 #Parameters:
-δ = 0.05    #depreciation rate
+δ = 1   #depreciation rate
 θ = 1/3  #capital share of output
-β = 0.975  #Discouting
-σ = 1  #Elasticity of Intertemporal Substitution
-ψ = 2    #Labor parameter
-γn= 0.003    #Population growth rate
-γz= 0.004   #Productivitu growth rate
-gss = 0.03 #average g
-τxss = 0.01 #average τx
-τhss = 0.02 #average τh
+β = 0.9  #Discouting
+σ = 2  #Elasticity of Intertemporal Substitution
+ψ = 0    #Labor parameter
+γn= 0.00    #Population growth rate
+γz= 0.00   #Productivitu growth rate
+gss = 0.0 #average g
+τxss = 0.0 #average τx
+τhss = 0.0 #average τh
 zss = 1 #average z
+
 
 #Parameters to be estimated
 ρg = 0.0
 ρx = 0.0
 ρh = 0.0
-ρz = 0.0
-σg= 0.00
-σx = 0.00
-σz = 0.00
-σh = 0.00
+ρz = 0.12
+σg= 0.0
+σx = 0.0
+σz = 1.0
+σh = 0.0
 
 #Function with the FOCs
 function SS!(eq, vector::Vector)
@@ -32,30 +33,36 @@ function SS!(eq, vector::Vector)
     g, τx,τh, z = gss,τxss,τhss, zss
     z1 = z
     τx1 = τx
-    c = k^θ * (z *h)^(1-θ) - ((1+γz)*(1+γn)*k1-(1-δ)*k+g)
+
+    c = k * ((z *h)^(1-θ))^(1/θ) - ((1+γz)*(1+γn)*k1-(1-δ)*k+ g )^(1/θ)
     c1 = c
-    eq[1] = ψ *c/(1-h) - (1-τh)*(1-θ) *(k/h)^θ *z^(1-θ)
-    eq[2] = c^(-σ) *(1-h)^(ψ*(1-σ))*(1+τx)  -  β*(1+γz)^(-σ) * c1^(-σ) * (1-h1)^(ψ*(1-σ)) *(θ*k1^(-θ)*(z1*h1)^(1-θ)+(1-δ)*(1+τx1))
+    eq[1] = (ψ *c)^(1/θ)  - (k/h)*((1-h)*(1-τh)*(1-θ)*z^(1-θ))^(1/θ)
+
+    eq[2] = (c^(-σ) *(1-h)^(ψ*(1-σ))*(1+τx)  - (1-δ)*(1+τx1)* β*(1+γz)^(-σ) * c1^(-σ) * (1-h1)^(ψ*(1-σ)))^(-1/θ) -
+     (β*(1+γz)^(-σ) * c1^(-σ) * (1-h1)^(ψ*(1-σ)) * θ*(z1*h1)^(1-θ))^(-1/θ)* k1
     return eq
 end
 
-SteadyState = nlsolve(SS!, [0.05,0.97],ftol = :1.0e-20, method = :trust_region , autoscale = true)
+kss = (θ*β)^(1/θ)
+
+SteadyState = nlsolve(SS!, [0.2,0.8],ftol = :1.0e-20, method = :trust_region , autoscale = true)
 kss,hss = SteadyState.zero
 
 
 function loglineq1(vector::Vector)
-    k,k1,h,z,τh,g= (vector)
+    k,k1,h,z,τh,g= vector
 
-    c = k^θ * (z *h)^(1-θ) - ((1+γz)*(1+γn)*k1-(1-δ)*k+g)
-    eq = ψ *c/(1-h) - (1-τh)*(1-θ) *(k/h)^θ *z^(1-θ)
+    c = k * ((z *h)^(1-θ))^(1/θ) - ((1+γz)*(1+γn)*k1-(1-δ)*k+ g )^(1/θ)
+    eq =(ψ *c)^(1/θ)  - (k/h)*((1-h)*(1-τh)*(1-θ)*z^(1-θ))^(1/θ)
 
     return eq
 end
 function loglineq2(vector::Vector)
     k,k1,k2,h,h1,z,τx,g,z1,τx1,g1 = (vector)
-    c = k^θ * (z *h)^(1-θ) - ((1+γz)*(1+γn)*k1-(1-δ)*k+g)
-    c1 = k1^θ * (z1 *h1)^(1-θ) - ((1+γz)*(1+γn)*k2-(1-δ)*k1+g1)
-    eq = c^(-σ) *(1-h)^(ψ*(1-σ))*(1+τx)  -  β*(1+γz)^(-σ) * c1^(-σ) * (1-h1)^(ψ*(1-σ)) *(θ*k1^(-θ)*(z1*h1)^(1-θ)+(1-δ)*(1+τx1))
+    c = k * ((z *h)^(1-θ))^(1/θ) - ((1+γz)*(1+γn)*k1-(1-δ)*k+ g )^(1/θ)
+    c1 = k * ((z1 *h1)^(1-θ))^(1/θ) - ((1+γz)*(1+γn)*k2-(1-δ)*k1+ g1 )^(1/θ)
+    eq =  (c^(-σ) *(1-h)^(ψ*(1-σ))*(1+τx)  - (1-δ)*(1+τx1)* β*(1+γz)^(-σ) * c1^(-σ) * (1-h1)^(ψ*(1-σ)))^(-1/θ) -
+     (β*(1+γz)^(-σ) * c1^(-σ) * (1-h1)^(ψ*(1-σ)) * θ*(z1*h1)^(1-θ))^(-1/θ)* k1
     return eq
 end
 
@@ -64,6 +71,9 @@ end
 T=ForwardDiff.gradient(loglineq1,[kss,kss,hss,zss,τhss,gss])
 a =[-kss*T[1]/(kss*T[1]),-kss*T[2]/(kss*T[1]),-hss*T[3]/(kss*T[1]),
 -zss*T[4]/(kss*T[1]),-τhss*T[5]/(kss*T[1]),-gss*T[6]/(kss*T[1])]
+#if ψ==0
+#    a[1],a[2:end]=-1,zeros(5)
+#end
 
 T=ForwardDiff.gradient(loglineq2,[kss,kss,kss,hss,hss,zss,τxss,gss,zss,τxss,gss])
 b = [kss*T[1]/(-kss*T[1]),kss*T[2]/(-kss*T[1]),kss*T[3]/(-kss*T[1]),hss*T[4]/(-kss*T[1]),
@@ -71,17 +81,32 @@ hss*T[5]/(-kss*T[1]),zss*T[6]/(-kss*T[1]),τxss*T[7]/(-kss*T[1]),gss*T[8]/(-kss*
 zss*T[9]/(-kss*T[1]),τxss*T[10]/(-kss*T[1]),gss*T[11]/(-kss*T[1])]
 
 A1 = [1 0 0; 0 0 0; 0 b[3] b[5]]
-A2 = [0 -1 0; a[1] a[2] a[3]; a[1] a[2] a[4]]
+A2 = [0 -1 0; a[1] a[2] a[3]; b[1] b[2] b[4]]
 U = [0 0 0 0 0 0 0 0;
 a[4] a[5] 0 a[6] 0 0 0 0;
 b[6] 0 b[7] b[8] b[9] 0 b[10] b[11]]
 
+A1,A2
 eig = eigen(A1,-A2)
 V=eig.vectors
+Π = eig.values
 #Sorting
-V[:,1], V[:,2] =V[:,2], V[:,1]
-Π = Diagonal([eig.values[2], eig.values[1], eig.values[3]])
-
+for j=1:3
+for i=1:2
+    if eps(Float64)<abs(Π[i+1])<abs(Π[i])
+        Π[i],Π[i+1] = Π[i+1],Π[i]
+        V[:,i],V[:,i+1] = V[:,i+1],V[:,i]
+    elseif abs(Π[i]) < eps(Float64)
+        Π[i],Π[end] =Π[end],Π[i]
+        V[:,i],V[:,end]=V[:,end],V[:,i]
+    end
+end
+end
+if abs(Π[1])>1
+    error("All Eigen Values outside unit circle")
+end
+Π= Diagonal(Π)
+iszero(Π[1])
 #If want to check if these matrics conform (they are equal but there is some roundoff error):
 A1*V
 -A2*V*Π
@@ -94,10 +119,11 @@ P = [ρg 0 0 0;
 0 ρx 0 0 ;
 0 0 ρh 0 ;
 0 0 0 ρz]
-D = [σg 0 0 0;
+Q = [σg 0 0 0;
 0 σx 0 0 ;
 0 0 σh 0 ;
 0 0 0 σz]
+
 
 function system!(eq,vector::Vector)
     #vector = rand(8)
@@ -105,8 +131,29 @@ function system!(eq,vector::Vector)
     B=vector[1:4]'
     D2 = vector[5:8]'
 
-    eq[1:4] = a[2].*B.+a[3].+a[3].*D2.+[a[4] a[5] 0 a[6]]
-    eq[5:8] = b[2].*B.+b[3].*A.*B+b[3].*B*P.+b[4].*D2.+b[5].*C[2].+b[5].*B*P.+[b[6] 0 b[7] b[8]].+[b[9] 0 b[10] b[1]]*P
+    eq[1:4] = a[2].*B .+ a[3].*D2 .+ [a[4] a[5] 0 a[6]]
+    eq[5:8] = b[2].*B .+ b[3].*A.*B .+ b[3].*B*P .+ b[4].*D2 .+ b[5].*C[2].*B .+ b[5].*B*P.+
+    [b[6] 0 b[7] b[8]].+[b[9] 0 b[10] b[11] ]*P
  return     eq
 end
-D = nlsolve(system!, [1.0,1,1,1,1,1,1,1],ftol = :1.0e-20, method = :trust_region , autoscale = true)
+
+
+Sol = nlsolve(system!, ones(8),ftol = :1.0e-20, method = :trust_region , autoscale = true)
+D=ones(2,4)
+D[1,:]= Sol.zero[1:4]
+D[2,:]= Sol.zero[5:8]
+
+
+T=100
+S= ones(4,T).* [0,0,0,zss]
+Z=ones(2,T).*[kss,hss]
+
+
+for t=2:T
+    S[:,t] = P*S[:,t-1]+Q*randn(4,1)
+    Z[:,t] = C*Z[1,t] + D*S[:,t]
+end
+
+plot([Z[1,:],Z[2,:]],labels = ["K","L"])
+
+#plot([S[1,:],S[4,:]],labels = ["K","L"])
