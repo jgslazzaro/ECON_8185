cbd29a09d63acb7cf49d1149b9aa13c295c31841
jgslazzaro <jgslazzaro@gmail.com>
2019-02-06 14:00:47 -0600

    HW4, question 1 works!
    Commenting more!

--- Ellen/HW4/main.jl
+++ Ellen/HW4/main.jl
@@ -3,12 +3,12 @@
 
 
 using Plots, Optim, LinearAlgebra, FastGaussQuadrature
+
 #Defining parameters
 θ = 0.25
 β = 0.9
 δ = 1
 A = (1-β*(1-δ))/(θ * β) #This will normalize the SS to 1
-
 kss = ((1- β*(1-δ))/(β*A*θ))^(1/(θ-1))
 
 #Finite elements Piecewise Linear function:
@@ -46,7 +46,6 @@ for i=2:length(K)
     K[i] = K[i-1] + 0.005*exp(0.574*(i-2))
 end
 
-
 #Defining consumption approximate function, this definition would hold for any
 #function to be approximated
 function cn(k,α;K=K)
@@ -60,16 +59,15 @@ function cn(k,α;K=K)
 
 #capital policy function from Bugdet constraint
 polk(k,α) = min(max(eps(),A*k.^θ+(1-δ)*k-cn(k,α)),K[end])
-
 #min max are needed to avoid NaNs and other numerical instabilities
-
 
 function residual(k,α) #Residual function comes from FOCs
         R = cn(k,α)/cn(polk(k,α),α) * β * (A*θ*polk(k,α)^(θ-1)+1-δ)- 1
     return R
 end
 
-#This function calculates the function that will be integrated.
+#This function calculates the function that will be integrated:
+#integra(k;α):= ϕi(k)R(k;α)
 function integra(k,α;K=K)
     T=zeros(length(K))
     for i=1:length(K)
@@ -79,7 +77,8 @@ function integra(k,α;K=K)
 end
 
 #This function calculates the integral (the norm of the integrated functions), as a functions of the parameters to minimized
-#We define that way since this is the format accepted by the solver
+#We define that way since this is the format accepted by the solver:
+#mini(α):= ∫integra(k;α)dk
 nodes, weights = gausslegendre(3*(length(K)-1)) #Gauss Legendre nodes and weights,this function is just a Quadrature table
 function mini(α;nodes=nodes,weights=weights,K=K)
     if length(α)<length(K)
@@ -95,14 +94,15 @@ function mini(α;nodes=nodes,weights=weights,K=K)
     return norm(gaussleg,1)
 end
 
-
 #Setting initial conditions
 initial =  ones(length(K)-1) .* range(0.35, stop = 3.5, length = length(K)-1)
 
 #Check if the initial conditions are somewhat close to the true parameters.
 mini(initial)
 
-#Solver stuff
+#Here we start the minimization procedure we want to find α:= argmin mini(α)
+
+#Solver stuff:
 #lower and upper bound of the parameters:
 lower = zeros(length(initial))
 upper = Inf*ones(length(initial))
@@ -113,7 +113,6 @@ inner_optimizer = BFGS()
 bla = optimize(mini,lower,upper,initial, Fminbox(inner_optimizer))
 #Parameters
 α = vcat(0,bla.minimizer)
-
 
 #Plotting
 k=K[1]:0.01:K[end]
