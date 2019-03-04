973cf137b879921df2a892d1bc2b76559719850a
jgslazzaro <jgslazzaro@gmail.com>
2019-03-04 11:54:16 -0600

    HW4 in progress
    I need to figure out how to include the capital forecast in the VFI algorithm

--- Anmol/HW4/functions.jl
+++ Anmol/HW4/functions.jl
@@ -1,31 +1,32 @@
 using Distributions, Optim, ApproxFun, NLsolve
+
 function Tauchen(ρ,σ,Y,μ = 0,m = 3)
     #This function is to discretize an AR(1) process following Tauchen(1986) method
     # y_{t+1} = μ + ρy_t + ϵ
     #ϵ~N(0,σ^2)
     #Y is the number of y states
-if Y>1
-    ybar = μ/(1-ρ)
-    ymax= ybar + m*(σ^2/(1-ρ^2))^(1\2) #maximum y
-    ymin= ybar - m*(σ^2/(1-ρ^2))^(1\2) #minimum y
+    if Y>1
+        ybar = μ/(1-ρ)
+        ymax= ybar + m*(σ^2/(1-ρ^2))^(1\2) #maximum y
+        ymin= ybar - m*(σ^2/(1-ρ^2))^(1\2) #minimum y
 
-    Δ = (ymax-ymin)/(Y-1)# #distance between each y
-    y=ymin:Δ:ymax #vector of possible states of p
+        Δ = (ymax-ymin)/(Y-1)# #distance between each y
+        y=ymin:Δ:ymax #vector of possible states of p
 
-    d=Normal()
+        d=Normal()
 
-    pdfY=ones(Y,Y) #preallocate memory and create the transition matrix in the following loop
-    for i in 1:Y
-        pdfY[i,1]=cdf(d,(y[1] + Δ/2 -ρ*y[i]) / σ^0.5);
-        pdfY[i,Y]=1-cdf(d,(y[Y] -Δ/2 - ρ*y[i]) / σ^0.5);
-        for j in 2:Y-1
-            pdfY[i,j]=cdf(d,(y[j] + Δ/2 - ρ*y[i])/σ^0.5) - cdf(d,(y[j] - Δ/2 - ρ*y[i]) / σ^0.5);
+        pdfY=ones(Y,Y) #preallocate memory and create the transition matrix in the following loop
+        for i in 1:Y
+            pdfY[i,1]=cdf(d,(y[1] + Δ/2 -ρ*y[i]) / σ^0.5);
+            pdfY[i,Y]=1-cdf(d,(y[Y] -Δ/2 - ρ*y[i]) / σ^0.5);
+            for j in 2:Y-1
+                pdfY[i,j]=cdf(d,(y[j] + Δ/2 - ρ*y[i])/σ^0.5) - cdf(d,(y[j] - Δ/2 - ρ*y[i]) / σ^0.5);
+            end
         end
+    else
+        y=μ
+        pdfY=1
     end
-else
-    y=μ
-    pdfY=1
-end
 
     return pdfY, y
 end
