1b4dd7221e44d781e63241a0526a86fceb17af4a
jgslazzaro <jgslazzaro@gmail.com>
2019-07-12 17:56:44 -0500

    HW6 working

---
+++ Anmol/HW6/functions.jl
@@ -0,0 +1,81 @@
+
+
+
+consumption(n,g) = n - g
+
+function V(n,γ,b,g,μ)
+    c = consumption(n,g)
+    UC = Uc(c,n)
+    UN = Un(c,n)
+    V = U(c,n) +μ*b*UC+γ*(UC*(c-b)+n*UN)
+    return V
+end
+
+
+nstar(γ,b,g,μ) = optimize(n-> -V(n,γ,b,g,μ) ,g,1.0).minimizer
+Vstar(γ,b,g,μ) = V(nstar(γ,b,g,μ),γ,b,g,μ)
+
+
+
+function EW(b1,μ1,g,Wgrid;Π=Π)
+
+
+    g0 = findfirst(G.==g)
+    bi = findfirst(B.==b1)
+    μi = findfirst(μ.==μ1)
+
+    expected::Float64 =0.0
+    for gi = 1:nG
+        expected += Π[g0,gi]*Wgrid[bi,μi,gi]
+    end
+    return expected
+end
+
+
+
+function SPFE(B,G,μ;nB=nB,nG=nG,nμ=nμ,β=β,Wgrid = Wgrid = ones(nB,nμ,nG))
+    #= Inputs:
+    Wgrid: Guess for value function
+    B: Grid for debt
+    G: Grid for government expenditures
+    μ: Grid for the Lagrange Multiplier
+    =#
+
+    #= Outputs:
+    Wgrid: Value Function
+    policy: Policy functions
+    =#
+
+    #Preallocating stuff
+    Wgrid1::Array{Float64,3}=copy(Wgrid) #to store updated values
+    objective::Array{Float64,2} = ones(nμ,nB) #This will be the function min maxed
+    policy::Array{Int64,4} = ones(Int64,nB,nμ,nG,2) #This will store the policy functions
+    dist::Float64 =1.0 #Distance
+    Vstargrid::Array{Float64,4} = ones(nB,nμ,nG,nμ) #This is the grid for the non0dynamic part of the SP problem
+    EWgrid::Array{Float64,3} = ones(nB,nμ,nG) #This is the grid the expected value function
+    #find the grid for the non dynamic part
+    for γi=1:nμ,gi = 1:nG, μi = 1:nμ , bi = 1:nB
+        Vstargrid[bi,μi,gi,γi] = Vstar(μ[γi],B[bi],G[gi],μ[μi])
+    end
+
+    while dist>1e-5
+        #find the grid of the new guess, for each possible value of γ and b1
+        for gi = 1:nG, γi = 1:nμ,b1 = 1:nB
+            EWgrid[b1,γi,gi] = EW(B[b1],μ[γi],G[gi],Wgrid)
+        end
+            for gi = 1:nG,μi = 1:nμ ,bi = 1:nB
+                #Joun the dynamic with non dynamic parts
+                for γi=1:nμ, b1i=1:nB
+                    objective[γi,b1i] = Vstargrid[bi,μi,gi,γi] +β * EWgrid[b1i,γi,gi]
+                end
+                ob2,indsB =findmax(objective,dims=2) #find the inner maximum of the SP problem
+                Wgrid1[bi,μi,gi],indsμ = findmin(ob2) #find the outer minimum
+                policy[bi,μi,gi,2] = indsμ[1] #get the policy indexes
+                policy[bi,μi,gi,1] = indsB[indsμ[1]][2]
+            end
+        dist = maximum(abs.(Wgrid.-Wgrid1)) #chech convergence
+        println("distance is $(dist)")
+        Wgrid = 0.75*Wgrid1+0.25*Wgrid
+    end
+    return Wgrid,policy
+end
--- Anmol/HW6/main.jl
+++ Anmol/HW6/main.jl
@@ -1,5 +1,7 @@
-using Optim, Interpolations
 
+#This code is for Anmol HW6
+using Optim, Interpolations
+include("functions.jl")
 #Defining parameters
 const β = 0.9
 const ψ = 0.69
@@ -10,9 +12,7 @@ const Θ = ones(2)
 # Derivatives of utility function
 U(c,n;ψ=ψ) = log(c) + ψ * log(1 - n)
 Uc(c,n) = 1 ./ c
-Ucc(c,n) = -c.^(-2.0)
 Un(c,n;ψ=ψ) = -ψ ./ (1.0 .- n)
-Unn(c,n;ψ=ψ) = -ψ ./ (1.0 .- n).^2.0
 
 
 nμ = 50
@@ -24,99 +24,14 @@ nΠ = length(Π)
 nG = length(G)
 
 
-Wgrid = ones(nB,nμ,nG)
-
-consumption(n,g) = n - g
-
-function V(n,γ,b,g,μ)
-    c = consumption(n,g)
-    UC = Uc(c,n)
-    UN = Un(c,n)
-    V = U(c,n) +μ*b*UC+γ*(UC*(c-b)+n*UN)
-    return V
-end
-
-#Γ = range(0,stop = 5,length = nΓ)
-
-nstar(γ,b,g,μ) = optimize(n-> -V(n,γ,b,g,μ) ,g,1.0).minimizer
-
-
-
-Vstar(γ,b,g,μ) = V(nstar(γ,b,g,μ),γ,b,g,μ)
-
-Vstar(0.1,0.1,0.1,.1)
-nstar(0.1,0.1,0.1,.1)
 #Guess for value function:
 
-itpW = LinearInterpolation((B,μ,G),Wgrid,extrapolation_bc=Line())
-W(b,μ,g) = itpW(b,μ,g)
+
+#Solve the saddle point problem:
+Wgrid,policy = SPFE(B,G,μ;nB=nB,nG=nG,nμ=nμ)
 
 
-function EW(b1,μ1,g;Π=Π)
-    g0 = findfirst(G.==g)
-    expected::Float64 =0.0
-    for gi = 1:nG
-        expected += Π[g0,gi]*W(b1,μ1,G[gi])
-    end
-    return expected
-end
-
-function EW(b1,μ1,g,Wgrid;Π=Π)
-    g0 = findfirst(G.==g)
-    bi = findfirst(B.==b1)
-    μi = findfirst(μ.==μ1)
-
-    expected::Float64 =0.0
-    for gi = 1:nG
-        expected += Π[g0,gi]*Wgrid[bi,μi,gi]
-    end
-    return expected
-end
-
-EW(B[1],μ[1],G[1],Wgrid)
-
-
-
-function SPFE(Wgrid::Array{Float64,3},B,G,μ;nB=nB,nG=nG,nμ=nμ,β=β )
-    Wgrid1::Array{Float64,3}=copy(Wgrid)
-    objective::Array{Float64,2} = ones(nμ,nB)
-    policy::Array{Int64,4} = ones(Int64,nB,nμ,nG,2)
-    dist::Float64 =1.0
-    Vstargrid::Array{Float64,4} = ones(nB,nμ,nG,nμ)
-    EWgrid::Array{Float64,3} = ones(nB,nμ,nG)
-
-        for γi=1:nμ,gi = 1:nG, μi = 1:nμ , bi = 1:nB
-            Vstargrid[bi,μi,gi,γi] = Vstar(μ[γi],B[bi],G[gi],μ[μi])
-        end
-
-
-    while dist>1e-5
-        #global Wgrid1,Wgrid,policy,dist,W
-        for gi = 1:nG, γi = 1:nμ,b1 = 1:nB
-            EWgrid[b1,γi,gi] = EW(B[b1],μ[γi],G[gi],Wgrid)
-        end
-            for gi = 1:nG,μi = 1:nμ ,bi = 1:nB
-                for γi=1:nμ, b1i=1:nB
-                    objective[γi,b1i] = Vstargrid[bi,μi,gi,γi] +β * EWgrid[b1i,γi,gi]
-                end
-                ob2,indsB =findmax(objective,dims=2)
-                Wgrid1[bi,μi,gi],indsμ = findmin(ob2)
-                policy[bi,μi,gi,2] = indsμ[1]
-                policy[bi,μi,gi,1] = indsB[indsμ[1]][2]
-            end
-        dist = maximum(abs.(Wgrid.-Wgrid1))
-        println("distance is $(dist)")
-        Wgrid = 0.75*Wgrid1+0.25*Wgrid
-    end
-    return Wgrid,policy
-end
-
-Wgrid,policy = SPFE(Wgrid,B,G,μ;nB=nB,nG=nG,nμ=nμ)
-
-maximum(policy[:,:,:,1])
-maximum(policy[:,:,:,2])
-
-
+#Simulate and plot the economy:
 T = 20
 Ghist = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1]
 b0 = .50
@@ -142,7 +57,7 @@ Ghist = G[Ghist]
 
 chist = nhist .-Ghist
 
-tauhist = 1 .- Un.(chist,nhist) ./ Uc.(chist,nhist)
+tauhist = 1 .+ Un.(chist,nhist) ./ Uc.(chist,nhist)
 
 using Plots
 
